29943bab8d9f25b6530bc530c4f6dcaf28d89bdc
Fiat Veritas <jesse.gallegos@usc.edu>
2021-07-15 16:58:00 -0700

    feat: removed redudant code

--- pseudo_quicksort.c
+++ pseudo_quicksort.c
@@ -3,9 +3,7 @@
 #include <math.h>
 #include <time.h>
 
-int *quicksort(int[], int);
 int *arrange(int[], const int, int);
-int exchange(int, int);
 int make_index(const int);
 
 int main(){
@@ -19,32 +17,28 @@ int main(){
 	printf("xxxxxxxxxxxx\n");
 	printf("size = %d\n", SIZE);
 	printf("xxxxxxxxxxxx\n");
+	printf("generating random array:\n");
 
 	for (int j = 0; j < SIZE; j++){
 		A[j] = rand() % 20;
-		printf("%d ", A[j]);
 	}
-
-	printf("\nxxxxxxxxxxxx\n");
+	printf("xxxxxxxxxxxx\n");
 	printf("index %d\n", index);
 	printf("pivot number %d\n", A[index]);
-	//printf("xxxxxxxxxxxx\n");
-	int *sorted;
-	sorted = quicksort(A, index);
-	//printf("%d \t %u \n", *sorted, sorted);
 	printf("xxxxxxxxxxxx\n");
+	printf("original array:\n");
+	for (int j = 0; j < SIZE; j++){
+		printf("%d ", A[j]);
+	}
+	printf("\nmodified array:\n");
 	int *ptr = arrange(A, SIZE, index);
 	for(int i = 0; i < SIZE; i++){
 		printf("%d ", ptr[i]);
 	}
+	printf("\n");
 	ptr = NULL;
-	sorted = NULL;
 
 	return 0;
-}
-
-int *quicksort(int A[], int index){
-	return &A[index];
 }
 
 int *arrange(int A[], const int SIZE, int index){
@@ -53,19 +47,14 @@ int *arrange(int A[], const int SIZE, int index){
 	int a = 0, b = SIZE - 1; //a = start of array, b = end of array
 	for (int i = 0; i < SIZE; i++){
 		B[i] = A[index];
-		//printf("%d\n", B[i]);
-		//printf("xxxxxxxxxxxx\n");
 	}
 	for(int i = 0; i < SIZE; i++){
-		//code here
 		if (A[i] < A[index]){
 			B[a] = A[i];
-			//printf("B less%d\n", B[a]);
 			a++;
 		}
 		else if ( A[i] > A[index]){
 			B[b] = A[i];
-			//printf("B greater%d\n", B[b]);
 			b--;
 		}
 		else{
@@ -73,18 +62,6 @@ int *arrange(int A[], const int SIZE, int index){
 		}
 	}
 	return B;
-}
-
-int exchange(int a, int b){
-	if (a < b){
-		return a;
-	}
-	else if (a > b){
-		return b;
-	}
-	else{
-		return a;
-	}
 }
 
 int make_index(const int SIZE){
